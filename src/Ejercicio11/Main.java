1114ae51ea5ac4bfaac25fa8a2c7f7f9328d0176
Andres Bravo <bravoandres1706@gmail.com>
2025-07-08 09:27:21 -0400

    Ejercicio 11

---
+++ src/Ejercicio11/Main.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicio11;
+
+/**
+ *
+ * @author Andres Bravo
+ */
+
+/*
+ * Crea un programa que comprueba si los paréntesis, llaves y corchetes
+ * de una expresión están equilibrados.
+ * - Equilibrado significa que estos delimitadores se abren y cieran
+ *   en orden y de forma correcta.
+ * - Paréntesis, llaves y corchetes son igual de prioritarios.
+ *   No hay uno más importante que otro.
+ * - Expresión balanceada: { [ a * ( c + d ) ] - 5 }
+ * - Expresión no balanceada: { a * ( c + d ) ] - 5 }
+ */
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        balanceado("{ ( ) [ a * ( c + d ) ] - 5 }");
+        //System.out.println(isSigno('{'));
+    }
+
+    public static void balanceado(String expresion) {
+        ArrayList<Character> signos = llenarArregloCaracteres(expresion);
+
+        if (signos.size() % 2 != 0) {
+            System.out.println("No esta balanceado");
+        } else {
+            int i = 0;
+            while (!signos.isEmpty()) {
+                int tamaño = signos.size();
+                char primero = signos.get(i);
+                char ultimo = signos.get(tamaño - 1);
+                if (Inverso(primero, ultimo)) {
+                    signos.remove(tamaño - 1);
+                    signos.remove(i);
+                } else {
+                    //signos.clear();
+                    System.out.println("El arreglo no es Balanceado");
+                    break;
+                }
+                System.out.println("el arreglo ahora es " + signos);
+            }
+            //System.out.println("El arreglo es balanceado");
+            if (signos.isEmpty()) {
+                System.out.println("El arreglo esta balanceado");
+            }
+        }
+    }
+
+    public static boolean Inverso(char elemento, char siguiente) {
+        if (elemento == '{' && siguiente == '}') {
+            return true;
+        } else if (elemento == '[' && siguiente == ']') {
+            return true;
+        } else if (elemento == '(' && siguiente == ')') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static ArrayList<Character> llenarArregloCaracteres(String expresion) {
+        ArrayList<Character> signos2 = new ArrayList<>();
+        for (int i = 0; i < expresion.length(); i++) {
+            char caracter = expresion.charAt(i);
+            if (isSigno(caracter)) {
+                signos2.add(caracter);
+            }
+        }
+        return signos2;
+    }
+
+    public static boolean isSigno(char a) {
+        if (a == '{' || a == '}' || a == '[' || a == ']' || a == '(' || a == ')') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
